41c927253b0449fc09a88e9d6fe13c707755db59
dominique <courregesdominique@gmail.com>
2024-07-13 19:33:39 -0300

    firstcommit

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,26 @@
-console.log(tempData);
+const cardsContainer = document.querySelector("#cardsContainer")
+
+function jstoHtmlCards(elementJS){
+    const div = document.createElement("div")
+    const h1 = document.createElement("h1")
+    const button = document.createElement("button")
+    const image = document.createElement("image")
+
+    image.src =elementJS.poster
+    image.classList.add("cardImage")
+    image.alt = elementJS.title
+
+    h1.innerHTML = elementJS.title
+    button.innerHTML = "Watch Now"
+
+    div.classList.add("card")
+    div.appendChild(image)
+    div.appendChild(h1)
+    div.appendChild(button)
+
+    return div
+}
+
+const arrayHtmlCards = tempData.map(jstoHtmlCards)
+
+arrayHtmlCards.forEach( (elementHtml) => cardsContainer.appendChild(elementHtml))
