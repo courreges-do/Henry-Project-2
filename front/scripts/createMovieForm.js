a15227d44a648d8345d25bbb48266975e962e5f8
dominique <courregesdominique@gmail.com>
2024-12-03 09:26:05 -0300

    prettier

--- front/scripts/createMovieForm.js
+++ front/scripts/createMovieForm.js
@@ -1,62 +1,61 @@
 const axios = require("axios");
 
-function formReset(){
-    const form = document.querySelector("form");
-    if (form){
-      form.reset();
-    }
+function formReset() {
+  const form = document.querySelector("form");
+  if (form) {
+    form.reset();
+  }
 }
 
 async function formSubmit() {
-    const form = document.querySelector("form");
-    if (form) {
-        form.addEventListener("submit", async (event) => {
-            event.preventDefault();
+  const form = document.querySelector("form");
+  if (form) {
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
 
-            const chosenGenres = [];
-            const checkboxes = document.querySelectorAll(".form-check-input");
-            checkboxes.forEach((checkbox) => {
-                if (checkbox.checked) {
-                    chosenGenres.push(checkbox.value);
-                }
-            });
-            if (chosenGenres.length == 0) {
-                alert("Please choose at least one genre");
-                return;
-            }
+      const chosenGenres = [];
+      const checkboxes = document.querySelectorAll(".form-check-input");
+      checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+          chosenGenres.push(checkbox.value);
+        }
+      });
+      if (chosenGenres.length == 0) {
+        alert("Please choose at least one genre");
+        return;
+      }
 
-            const newMovieData = {
-                title: document.getElementById("titleInput").value.trim(),
-                genre: chosenGenres,
-                director: document.getElementById("directorInput").value.trim(),
-                rate: Number(document.getElementById("rateInput").value.trim()),
-                year: Number(document.getElementById("yearInput").value.trim()),
-                duration: document.getElementById("durationInput").value.trim(),
-                poster: document.getElementById("posterInput").value.trim(),
-            };
+      const newMovieData = {
+        title: document.getElementById("titleInput").value.trim(),
+        genre: chosenGenres,
+        director: document.getElementById("directorInput").value.trim(),
+        rate: Number(document.getElementById("rateInput").value.trim()),
+        year: Number(document.getElementById("yearInput").value.trim()),
+        duration: document.getElementById("durationInput").value.trim(),
+        poster: document.getElementById("posterInput").value.trim(),
+      };
 
-            try {
-                const response = await axios.post("http://localhost:3000/movies/", newMovieData);
-                console.log("Movie created successfully:", response.data);
-                alert("Movie created successfully");
-            } catch (error) {
-                if (error.response) {
-                    console.error("Error while creating movie:", error.response.data);
-                    alert("Error while creating movie: " + error.response.data.message);
-                } else {
-                    console.error("Error while creating movie:", error.message);
-                    alert("Error while creating movie");
-                }
-            }
-            
-        });
-    }
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/movies/",
+          newMovieData
+        );
+        console.log("Movie created successfully:", response.data);
+        alert("Movie created successfully");
+      } catch (error) {
+        if (error.response) {
+          console.error("Error while creating movie:", error.response.data);
+          alert("Error while creating movie: " + error.response.data.message);
+        } else {
+          console.error("Error while creating movie:", error.message);
+          alert("Error while creating movie");
+        }
+      }
+    });
+  }
 }
-
-
-
 
 module.exports = {
-    formReset,
-    formSubmit
-}
+  formReset,
+  formSubmit,
+};
--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,38 +1,33 @@
 const tempData = require("./tempData.js");
 const jstoHtmlCards = require("./jstoHtmlCards.js");
 const axios = require("axios");
-const { formReset, formSubmit } = require("./createMovieForm.js")
+const { formReset, formSubmit } = require("./createMovieForm.js");
 
-const cardsContainer = document.querySelector("#cardsContainer")
+const cardsContainer = document.querySelector("#cardsContainer");
 
-async function getMovies(){
+async function getMovies() {
   try {
-    const {data} = await axios.get("http://localhost:3000/movies");
-    const arrayMovies = data.data;  
+    const { data } = await axios.get("http://localhost:3000/movies");
+    const arrayMovies = data.data;
     const arrayHtmlCards = arrayMovies.map(jstoHtmlCards);
-    arrayHtmlCards.forEach((elementHtml) => 
+    arrayHtmlCards.forEach((elementHtml) =>
       cardsContainer.appendChild(elementHtml)
     );
   } catch (error) {
     alert("Error in the API");
     const arrayHtmlCards = tempData.map(jstoHtmlCards);
-    arrayHtmlCards.forEach( (elementHtml) => 
+    arrayHtmlCards.forEach((elementHtml) =>
       cardsContainer.appendChild(elementHtml)
     );
   }
 }
 
-if(window.location.pathname.endsWith("front/index.html")) {
+if (window.location.pathname.endsWith("front/index.html")) {
   getMovies();
 } else {
   const form = document.querySelector("form");
-  if (window.location.pathname.endsWith("front/pages/form.html")){
+  if (window.location.pathname.endsWith("front/pages/form.html")) {
     formSubmit();
     formReset();
   }
 }
-
-
-
-
-  
